3c144ebf6336c1e0c30f17a092b62d5983777671
Hernan Verzini <99434648+hernanver@users.noreply.github.com>
2022-08-11 10:18:38 -0300

    Create conjuntos.js

---
+++ conjuntos.js
@@ -0,0 +1,16 @@
+const array = [
+    "Mariano", 
+    "Laura",
+    "Juan",
+]
+const miSet = new Set(array)
+
+console.log(miSet)
+
+miSet.add("Hernan")
+
+console.log(miSet)
+
+miSet.add("Javascript")
+
+console.log(miSet)
