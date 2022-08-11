b13d438b6244dcf7fb3c3a27c1a8088aa4a2fac2
Hernan Verzini <99434648+hernanver@users.noreply.github.com>
2022-08-11 11:03:02 -0300

    Create fechas.js

---
+++ fechas.js
@@ -0,0 +1,9 @@
+const fecha = new Date()
+
+const fecha_nacimiento = new Date(1994,02,08)
+
+var comparacion = fecha > fecha_nacimiento
+
+var dia = fecha_nacimiento.getDate()
+var mes = (fecha_nacimiento.getMonth()+1)
+var año = fecha_nacimiento.getFullYear()
